fcf42211d36c8149ecd5cbacca9aab821e65736b
victoriashopify <edugpucheta@gmail.com>
2025-03-06 18:49:54 -0300

    feat: load widget iframe on window load and update widget URL

--- public/embed/widget.js
+++ public/embed/widget.js
@@ -1,14 +1,17 @@
 (function () {
-    const iframe = document.createElement("iframe");
-    iframe.src = "https://shipfast-template-supabse-k6pc.vercel.app/widget";
-    iframe.width = "400";
-    iframe.height = "500";
-    iframe.style.border = "none";
-    iframe.style.position = "fixed";
-    iframe.style.bottom = "20px";
-    iframe.style.right = "20px";
-    iframe.style.zIndex = "9999";
-    iframe.allow = "clipboard-write"; // Optional permissions
-    document.body.appendChild(iframe);
+    window.onload = function () {
+      const iframe = document.createElement("iframe");
+      iframe.src = "https://www.man-gos.com/widget"; // Your widget URL
+      iframe.width = "400";
+      iframe.height = "500";
+      iframe.style.border = "none";
+      iframe.style.position = "fixed";
+      iframe.style.bottom = "20px";
+      iframe.style.right = "20px";
+      iframe.style.zIndex = "9999";
+      iframe.allow = "clipboard-write"; // Optional permissions
+      document.body.appendChild(iframe);
+      console.log("Widget Loaded ✅");
+    };
   })();
   
