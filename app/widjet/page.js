2edcc311382bf236a52389e5c378f66f342d9c58
victoriashopify <eduardo@dynamicdata.com>
2025-03-21 17:15:07 -0300

    refactor: update WidgetPage layout for improved positioning and adjust iframe structure in iframe.html for better integration

--- app/widjet/page.js
+++ app/widjet/page.js
@@ -1,15 +1,12 @@
 "use client";
 import PreviewSurvey from "@/components/PreviewSurvey";
 
-
 export default function WidgetPage() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen h-80 absolute top-0 left-0 w-full z-50">
-   
-        <div id="root">
-          <PreviewSurvey isPreview={false} surveyID={42} />
-        </div>
-     
+    <div className="fixed inset-0 flex flex-col items-center justify-center min-h-screen h-80 w-full z-[9999]">
+      <div id="root" className="relative">
+        <PreviewSurvey isPreview={false} surveyID={42} />
+      </div>
     </div>
   );
 }
