1ab2cea0068d12493cc5763dba540d736f6f0a11
victoriashopify <eduardo@dynamicdata.com>
2025-03-21 14:08:42 -0300

    refactor: simplify WidgetPage structure by removing HTML tags and applying Tailwind CSS for improved styling

--- app/widjet/page.js
+++ app/widjet/page.js
@@ -3,32 +3,11 @@ import PreviewSurvey from "@/components/PreviewSurvey";
 
 export default function WidgetPage() {
   return (
-    <html>
-      <head>
-        <meta charSet="UTF-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <title>Feedback Widget</title>
-        <style>
-          {`
-            body {
-              margin: 0;
-              padding: 0;
-              overflow: hidden;
-              display: flex;
-              justify-content: center;
-              align-items: center;
-              height: 100vh;
-              background-color: white;
-            }
-          `}
-        </style>
-      </head>
-      <body>
-        <div>Survey widget2 </div>
-        <div id="root">
-          <PreviewSurvey />
-        </div>
-      </body>
-    </html>
+    <div className="flex flex-col items-center justify-center min-h-screen bg-white h-80">
+      <div>Survey widget2</div>
+      <div id="root">
+        <PreviewSurvey />
+      </div>
+    </div>
   );
 }
