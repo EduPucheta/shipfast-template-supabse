218bc4d2b8a3c46ce195a97c439b39b10d5f9c5c
victoriashopify <eduardo@dynamicdata.com>
2025-03-21 16:06:51 -0300

    feat: implement survey loading notification and adjust iframe dimensions for improved display in widget.js

--- app/widjet/page.js
+++ app/widjet/page.js
@@ -1,13 +1,16 @@
 "use client";
 import PreviewSurvey from "@/components/PreviewSurvey";
+import { useEffect } from "react";
 
 export default function WidgetPage() {
+  useEffect(() => {
+    // Notify the parent window that the survey is loaded
+    window.parent.postMessage({ type: 'surveyLoaded' }, '*');
+  }, []);
+
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen h-80" >
-      
-      <div id="root">
-        <PreviewSurvey isPreview={false} surveyID={42}  /> 
-      </div>
+    <div className="w-full h-screen">
+      <PreviewSurvey isPreview={false} surveyID={42} />
     </div>
   );
 }
