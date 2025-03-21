c21a60bc9a5650a8e5975cdf5a2101d5c821e0ac
victoriashopify <eduardo@dynamicdata.com>
2025-03-21 13:59:49 -0300

    fix: update survey widget text and adjust iframe dimensions for better display

--- app/widjet/page.js
+++ app/widjet/page.js
@@ -24,7 +24,7 @@ export default function WidgetPage() {
         </style>
       </head>
       <body>
-        <div>Survey widget</div>
+        <div>Survey widget2 </div>
         <div id="root">
           <PreviewSurvey />
         </div>
--- public/embed/widget.js
+++ public/embed/widget.js
@@ -2,8 +2,8 @@
     window.onload = function () {
       const iframe = document.createElement("iframe");
       iframe.src = "http://localhost:3000/widjet"; // Your widget URL
-      iframe.width = "auto" ;
-      iframe.height = "auto";
+      iframe.width = "200px" ;
+      iframe.height = "400px";
       iframe.style.border = "none";
       iframe.style.position = "fixed";
       iframe.style.bottom = "20px";
