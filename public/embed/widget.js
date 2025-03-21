49cc640101b7f98910ec9039c94926d047e5e226
victoriashopify <eduardo@dynamicdata.com>
2025-03-21 13:11:05 -0300

    fix: update widget iframe URL to local development and adjust dimensions to auto

--- public/embed/widget.js
+++ public/embed/widget.js
@@ -1,9 +1,9 @@
 (function () {
     window.onload = function () {
       const iframe = document.createElement("iframe");
-      iframe.src = "https://shipfast-template-supabse-k6pc.vercel.app/widjet"; // Your widget URL
-      iframe.width = "400";
-      iframe.height = "500";
+      iframe.src = "http://localhost:3000/widjet"; // Your widget URL
+      iframe.width = "auto" ;
+      iframe.height = "auto";
       iframe.style.border = "none";
       iframe.style.position = "fixed";
       iframe.style.bottom = "20px";
