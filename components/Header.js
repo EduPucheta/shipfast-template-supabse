54a372caf60e5522f3a5ce291c72877d4a307334
EduPucheta <edugpucheta@gmail.com>
2025-07-19 17:31:16 -0300

    chore: update daisyui dependency and clean up header component
    - Upgraded daisyui from version 5.0.9 to 5.0.46 in package.json and package-lock.json.
    - Removed unnecessary class from the header component for cleaner markup.

--- components/Header.js
+++ components/Header.js
@@ -30,7 +30,7 @@ const Header = () => {
   }, [searchParams]);
 
   return (
-    <header className="bg-base-200">
+    <header >
       <nav
         className="container flex items-center justify-between px-8 py-4 mx-auto"
         aria-label="Global"
