015e25a18ab58c974f95675ef993ddf6f5b46947
EduPucheta <edugpucheta@gmail.com>
2025-07-27 22:25:07 -0300

    feat: integrate ConditionalWidget and enhance widget URL handling
    - Added ConditionalWidget component to layout.js for improved feature toggling.
    - Updated widget.js to dynamically determine the base URL for the widget, enhancing compatibility across environments.
    - Adjusted iframe dimensions in widget.js for better responsiveness and user experience.

--- public/widget.js
+++ public/widget.js
@@ -2,9 +2,12 @@
   const currentScript = document.currentScript;
   const spaceId = currentScript.src.split('space_id=')[1];
 
+  // Dynamically determine the base URL based on the current script's source
+  const scriptUrl = new URL(currentScript.src);
+  const baseUrl = `${scriptUrl.protocol}//${scriptUrl.host}`;
 
   const iframe = document.createElement('iframe');
-  const widgetUrl = new URL('https://localhost:3000/widjet');
+  const widgetUrl = new URL(`${baseUrl}/widjet`);
   
   widgetUrl.searchParams.set('pageUrl', window.location.href);
   widgetUrl.searchParams.set('browser', navigator.userAgent);
@@ -16,7 +19,7 @@
   iframe.style.bottom = '20px';
   iframe.style.right = '20px';
   iframe.style.border = 'none';
-  iframe.style.width = 'auto';
+  iframe.style.width = '124px';
   iframe.style.height = '40px'; 
   iframe.style.transition = 'height 0.3s ease, width 0.3s ease';
   iframe.style.zIndex = '9999';
@@ -26,13 +29,13 @@
   document.body.appendChild(iframe);
 
   window.addEventListener('message', (event) => {
-    if (event.origin !== 'https://localhost:3000') return;
+    if (event.origin !== baseUrl) return;
 
     if (event.data.type === 'expand-widget') {
       iframe.style.width = '350px';
-      iframe.style.height = '480px';
+      iframe.style.height = '366px';
     } else if (event.data.type === 'collapse-widget') {
-      iframe.style.width = 'auto';
+      iframe.style.width = '124px';
       iframe.style.height = '40px';
     } else if (event.data.type === 'widget-height-change') {
       iframe.style.height = `${event.data.height}px`;
